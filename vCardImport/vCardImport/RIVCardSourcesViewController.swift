38047ba595b7c1a274765b35568329af5c0dc2f0
Tuomas Kareinen <tkareine@gmail.com>
2014-10-13 22:42:06 +0300

    Begin implementing the table view controller

--- vCardImport/vCardImport/RIVCardSourcesViewController.swift
+++ vCardImport/vCardImport/RIVCardSourcesViewController.swift
@@ -3,11 +3,22 @@ import UIKit
 class RIVCardSourcesViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCell")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("UITableViewCell", forIndexPath: indexPath) as UITableViewCell
+        cell.textLabel?.text = "oh my"
+        return cell
     }
 }
