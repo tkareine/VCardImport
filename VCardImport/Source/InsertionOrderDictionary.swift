08b5bb06dd8f38dad006ba5b2e5c829e76efebd9
Tuomas Kareinen <tkareine@gmail.com>
2015-01-11 00:44:53 +0200

    Introduce dictionary with insertion order semantics

---
+++ VCardImport/Source/InsertionOrderDictionary.swift
@@ -0,0 +1,127 @@
+struct InsertionOrderDictionary<K: Hashable, V> {
+  private var keyOrder: [K] = []
+  private var dictionary: [K: V] = [:]
+
+  init() {}
+
+  var isEmpty: Bool {
+    return keyOrder.isEmpty
+  }
+
+  var count: Int {
+    return keyOrder.count
+  }
+
+  var keys: [K] {
+    return keyOrder
+  }
+
+  var values: [V] {
+    return Array(self).map { $1 }
+  }
+
+  subscript(key: K) -> V? {
+    get {
+      return dictionary[key]
+    }
+
+    set {
+      if let existing = dictionary[key] {
+        dictionary[key] = newValue
+      } else {
+        keyOrder.append(key)
+        dictionary[key] = newValue
+      }
+    }
+  }
+
+  subscript(index: Int) -> V {
+    get {
+      let key = keyOrder[index]
+      return dictionary[key]!
+    }
+
+    set {
+      let key = keyOrder[index]
+      dictionary[key] = newValue
+    }
+  }
+
+  mutating func removeValueForKey(key: K) -> V? {
+    if let value = dictionary[key] {
+      let index = find(keyOrder, key)!
+      return remove(index, key)
+    } else {
+      return nil
+    }
+  }
+
+  mutating func removeValueAtIndex(index: Int) -> V? {
+    if index < keyOrder.count {
+      let key = keyOrder[index]
+      return remove(index, key)
+    } else {
+      return nil
+    }
+  }
+
+  private mutating func remove(index: Int, _ key: K) -> V {
+    keyOrder.removeAtIndex(index)
+    return dictionary.removeValueForKey(key)!
+  }
+
+  func indexOf(key: K) -> Int? {
+    return find(keyOrder, key)
+  }
+}
+
+struct InsertionOrderDictionaryGenerator<K: Hashable, V>: GeneratorType {
+  private var keyOrder: [K]
+  private var dictionary: [K: V]
+
+  typealias Element = (K, V)
+
+  mutating func next() -> Element? {
+    if keyOrder.isEmpty {
+      return nil
+    }
+    let key = keyOrder[0]
+    let value = dictionary[key]!
+    keyOrder = Array(keyOrder[1..<keyOrder.count])
+    dictionary.removeValueForKey(key)
+    return (key, value)
+  }
+}
+
+extension InsertionOrderDictionary: SequenceType {
+  typealias Generator = InsertionOrderDictionaryGenerator<K, V>
+
+  func generate() -> Generator {
+    return InsertionOrderDictionaryGenerator(
+      keyOrder: keyOrder,
+      dictionary: dictionary)
+  }
+}
+
+extension InsertionOrderDictionary: DictionaryLiteralConvertible {
+  typealias Key = K
+  typealias Value = V
+
+  init(dictionaryLiteral elements: (Key, Value)...) {
+    for (key, value) in elements {
+      keyOrder.append(key)
+      dictionary[key] = value
+    }
+  }
+}
+
+extension InsertionOrderDictionary: Printable {
+  var description: String {
+    return join(map(self) { k, v in "\(k): \(v)" })
+  }
+
+  private func join(pairs: [String]) -> String {
+    let joined = ", ".join(pairs)
+    return "[" + joined + "]"
+  }
+}
---
+++ VCardImport/Test/InsertionOrderDictionaryTests.swift
@@ -0,0 +1,121 @@
+import XCTest
+
+class InsertionOrderDictionaryTests: XCTestCase {
+  func testEmpty() {
+    var dict: InsertionOrderDictionary<String, Int> = InsertionOrderDictionary()
+
+    XCTAssertTrue(dict.isEmpty)
+    XCTAssertEqual(dict.count, 0)
+    XCTAssertEqual(dict.description, "[]")
+    XCTAssertEqual(dict.keys, [])
+    XCTAssertEqual(dict.values, [])
+  }
+
+  func testAdd() {
+    var dict: InsertionOrderDictionary<String, Int> = InsertionOrderDictionary()
+    dict["a"] = 0
+    dict["b"] = 1
+
+    XCTAssertFalse(dict.isEmpty)
+    XCTAssertEqual(dict.count, 2)
+    XCTAssertEqual(dict.description, "[a: 0, b: 1]")
+    XCTAssertEqual(dict.keys, ["a", "b"])
+    XCTAssertEqual(dict.values, [0, 1])
+    XCTAssertEqual(dict["a"]!, 0)
+    XCTAssertEqual(dict[0], 0)
+    XCTAssertEqual(dict["b"]!, 1)
+    XCTAssertEqual(dict[1], 1)
+  }
+
+  func testModify() {
+    var dict: InsertionOrderDictionary<String, Int> = InsertionOrderDictionary()
+    dict["a"] = 0
+    dict["b"] = 1
+    dict[1] = 11
+    dict[0] = 10
+
+    XCTAssertEqual(dict.count, 2)
+    XCTAssertEqual(dict["a"]!, 10)
+    XCTAssertEqual(dict["b"]!, 11)
+  }
+
+  func testRemove() {
+    var dict: InsertionOrderDictionary<String, Int> = InsertionOrderDictionary()
+    dict["a"] = 0
+    dict["b"] = 1
+
+    XCTAssertEqual(dict["a"]!, 0)
+
+    let r0 = dict.removeValueAtIndex(0)
+
+    XCTAssertEqual(r0!, 0)
+    XCTAssertEqual(dict.count, 1)
+    XCTAssert(dict["a"] == nil)
+    XCTAssertEqual(dict[0], 1)
+    XCTAssertEqual(dict["b"]!, 1)
+
+    let r1 = dict.removeValueForKey("a")
+
+    XCTAssert(r1 == nil)
+
+    let r2 = dict.removeValueForKey("b")
+
+    XCTAssertEqual(r2!, 1)
+    XCTAssertTrue(dict.isEmpty)
+    XCTAssert(dict["b"] == nil)
+  }
+
+  func testIndexOf() {
+    var dict: InsertionOrderDictionary<String, Float> = InsertionOrderDictionary()
+    dict["a"] = 3.14
+    dict["b"] = 42.0
+
+    XCTAssertEqual(dict.indexOf("a")!, 0)
+    XCTAssertEqual(dict.indexOf("b")!, 1)
+    XCTAssert(dict.indexOf("c") == nil)
+  }
+
+  func testGenerate() {
+    var dict: InsertionOrderDictionary<String, Int> = InsertionOrderDictionary()
+    dict["a"] = 0
+    dict["b"] = 1
+    dict["c"] = 2
+
+    var gen0 = dict.generate()
+    let t0_0 = gen0.next()!
+
+    XCTAssertEqual(t0_0.0, "a")
+    XCTAssertEqual(t0_0.1, 0)
+
+    dict.removeValueAtIndex(1)
+    dict["c"] = 3
+
+    let t0_1 = gen0.next()!
+    var gen1 = dict.generate()
+    let t1_0 = gen1.next()!
+
+    XCTAssertEqual(t0_1.0, "b")
+    XCTAssertEqual(t0_1.1, 1)
+    XCTAssertEqual(t1_0.0, "a")
+    XCTAssertEqual(t1_0.1, 0)
+
+    let t0_2 = gen0.next()!
+    let t1_1 = gen1.next()!
+
+    XCTAssertEqual(t0_2.0, "c")
+    XCTAssertEqual(t0_2.1, 2)
+    XCTAssertEqual(t1_1.0, "c")
+    XCTAssertEqual(t1_1.1, 3)
+
+    let t0_3 = gen0.next()
+    let t1_2 = gen1.next()
+
+    XCTAssert(t0_3 == nil)
+    XCTAssert(t1_2 == nil)
+  }
+
+  func testLiteral() {
+    let dict: InsertionOrderDictionary<String, Int> = ["a": 0, "b": 1, "c": 2, "d": 3]
+    XCTAssertEqual(dict.description, "[a: 0, b: 1, c: 2, d: 3]")
+  }
+}
