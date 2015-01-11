246fdbdc3ed61efe1868f34b7b8c55d1d1cbef3a
Tuomas Kareinen <tkareine@gmail.com>
2015-01-11 03:07:52 +0200

    Reimplement vcard source store with InsertionOrderDictionary

--- VCardImport/Source/VCardSourceStore.swift
+++ VCardImport/Source/VCardSourceStore.swift
@@ -1,50 +1,44 @@
 import Foundation
 
 class VCardSourceStore {
-  private var sourceIds: [String] = []
-  private var sourcesByIds: [String: VCardSource] = [:]
+  private var store: InsertionOrderDictionary<String, VCardSource> = InsertionOrderDictionary()
 
   var isEmpty: Bool {
-    return sourceIds.isEmpty
+    return store.isEmpty
   }
 
   var countAll: Int {
-    return sourceIds.count
+    return store.count
   }
 
   var countEnabled: Int {
-    return countWhere(sourcesByIds.values, { $0.isEnabled })
+    return countWhere(store.values, { $0.isEnabled })
   }
 
   var filterEnabled: [VCardSource] {
-    return filter(sourcesByIds.values, { $0.isEnabled })
+    return filter(store.values, { $0.isEnabled })
   }
 
   init() {}
 
   subscript(index: Int) -> VCardSource {
-    get {
-      let sourceId = sourceIds[index]
-      return sourcesByIds[sourceId]!
-    }
+    return store[index]
   }
 
   func indexOf(source: VCardSource) -> Int {
-    return find(sourceIds, source.id)!
+    return store.indexOf(source.id)!
   }
 
   func update(source: VCardSource) {
-    sourcesByIds[source.id] = source
+    store[source.id] = source
   }
 
   func remove(index: Int) {
-    let sourceId = sourceIds[index]
-    sourceIds.removeAtIndex(index)
-    sourcesByIds.removeValueForKey(sourceId)
+    store.removeValueAtIndex(index)
   }
 
   func save() {
-    let sourcesData = JSONSerialization.encode(snapshotStore().map { $0.toDictionary() })
+    let sourcesData = JSONSerialization.encode(store.values.map { $0.toDictionary() })
     let defaults = NSUserDefaults.standardUserDefaults()
     defaults.setInteger(1, forKey: "VCardSourcesVersion")
     defaults.setObject(sourcesData, forKey: "VCardSources")
@@ -55,9 +49,9 @@ class VCardSourceStore {
     if let sourcesData = NSUserDefaults.standardUserDefaults().objectForKey("VCardSources") as? NSData {
       let sources = (JSONSerialization.decode(sourcesData) as [[String: AnyObject]])
         .map { VCardSource.fromDictionary($0) }
-      resetStore(sources)
+      resetFrom(sources)
     } else {
-      resetStore([
+      resetFrom([
         VCardSource(
           name: "Example: Body Corp",
           connection: VCardSource.Connection(url: NSURL(string: "https://dl.dropboxusercontent.com/u/1404049/vcards/bodycorp.vcf")!),
@@ -72,24 +66,11 @@ class VCardSourceStore {
     }
   }
 
-  private func snapshotStore() -> [VCardSource] {
-    var sources: [VCardSource] = []
-    for id in sourceIds {
-      sources.append(sourcesByIds[id]!)
-    }
-    return sources
-  }
-
-  private func resetStore(sources: [VCardSource]) {
-    var sourceIds: [String] = []
-    var sourcesByIds: [String: VCardSource] = [:]
-
+  private func resetFrom(sources: [VCardSource]) {
+    var store: InsertionOrderDictionary<String, VCardSource> = InsertionOrderDictionary()
     for source in sources {
-      sourceIds.append(source.id)
-      sourcesByIds[source.id] = source
+      store[source.id] = source
     }
-
-    self.sourceIds = sourceIds
-    self.sourcesByIds = sourcesByIds
+    self.store = store
   }
 }
