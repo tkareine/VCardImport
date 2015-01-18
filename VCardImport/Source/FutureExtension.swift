cedc3a3ea42edca710a2347d7b4b8d2c7a4d07c4
Tuomas Kareinen <tkareine@gmail.com>
2015-01-18 05:25:00 +0200

    Capitalize HTTP response description

--- VCardImport/Source/FutureExtension.swift
+++ VCardImport/Source/FutureExtension.swift
@@ -2,7 +2,7 @@ import Foundation
 
 extension PromiseFuture {
   func reject(response: NSHTTPURLResponse) {
-    let statusDesc = NSHTTPURLResponse.localizedStringForStatusCode(response.statusCode)
+    let statusDesc = NSHTTPURLResponse.localizedStringForStatusCode(response.statusCode).capitalized
     reject("\(statusDesc) (\(response.statusCode))")
   }
 }
