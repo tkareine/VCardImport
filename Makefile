434cea32b516e2e0f6da323afd05f32f7ef64f5d
Tuomas Kareinen <tkareine@gmail.com>
2015-01-24 17:30:54 +0200

    Add reverse proxy testing tool

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
 DSTROOT ?= .
 INSTALL_PATH ?= /build
 
-.PHONY: test clean
+.PHONY: test clean rproxy
 
 test:
 	xcodebuild -scheme VCardImport -target Test -destination 'platform=iOS Simulator,name=iPhone 5s,OS=latest' -destination-timeout 10 test
 
 clean:
 	rm -fr build DerivedData
+
+rproxy:
+	cd Support && bundle exec ruby rproxy.rb
---
+++ Support/rproxy.rb
@@ -0,0 +1,92 @@
+require "rack"
+require "rack/reverse_proxy"
+require "uri"
+
+class Config
+  attr_reader :uri, :basic_auth_username, :basic_auth_password, :delete_cache_headers
+
+  def initialize
+    @uri = get_uri_from_env
+    @basic_auth_username, @basic_auth_password = get_basic_auth_from_env
+    @delete_cache_headers = get_delete_cache_headers_from_env
+  end
+
+  def base_uri
+    "#{uri.scheme}://#{uri.host}/"
+  end
+
+  def use_basic_auth?
+    basic_auth_username != nil
+  end
+
+  private
+
+  def get_uri_from_env
+    URI.parse(ENV.fetch("RPROXY_URL", "https://dl.dropboxusercontent.com/u/1404049/vcards"))
+  end
+
+  def get_basic_auth_from_env
+    env = ENV.fetch("RPROXY_BASIC_AUTH", "")
+    if env
+      env.split(":", 2)
+    else
+      nil
+    end
+  end
+
+  def get_delete_cache_headers_from_env
+    ENV["RPROXY_DELETE_CACHE_HEADERS"] =~ /1|(?:true)/
+  end
+end
+
+class DumpHeaders
+  def initialize(app, options = {})
+    @app = app
+    @delete_cache_headers = options.fetch(:delete_cache_headers, false)
+  end
+
+  def call(env)
+    dump_http_headers("> ", env.select { |key, _| key =~ /HTTP_/ })
+    status, headers, response = @app.call(env)
+    delete_cache_headers(headers) if @delete_cache_headers
+    dump_http_headers("< ", headers)
+    [status, headers, response]
+  end
+
+  def dump_http_headers(prefix, headers)
+    headers.each do |key, value|
+      puts "#{prefix}#{key}: #{value}"
+    end
+  end
+
+  def delete_cache_headers(headers)
+    headers.delete_if do |key, _|
+      key =~ /^(?:etag|modified-since)/i
+    end
+  end
+end
+
+config = Config.new
+
+app = ->(_) { [404, {'Content-Type' => 'text/plain'}, ["not reverse proxied"]] }
+
+stack = Rack::Builder.new do
+  use Rack::CommonLogger
+  use DumpHeaders, delete_cache_headers: config.delete_cache_headers
+
+  if config.use_basic_auth?
+    use Rack::Auth::Basic, "RProxy Realm" do |username, password|
+      password == config.basic_auth_password
+    end
+  end
+
+  use Rack::ReverseProxy do
+    reverse_proxy(config.uri.path, config.base_uri)
+  end
+
+  use Rack::ShowExceptions
+
+  run app
+end
+
+Rack::Handler::WEBrick.run(stack, Port: 8080)
