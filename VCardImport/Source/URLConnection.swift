91103a2e78ec17b051e4627affa1a1ed57e9776e
Tuomas Kareinen <tkareine@gmail.com>
2015-01-24 22:05:39 +0200

    Reimplement URLConnection with Alamofire

--- VCardImport/Source/URLConnection.swift
+++ VCardImport/Source/URLConnection.swift
@@ -1,119 +1,109 @@
 import Foundation
+import Alamofire
 
 class URLConnection {
   typealias Headers = [String: String]
 
+  private let DefaultHeaders = [
+    "User-Agent": "\(Config.Executable)/\(Config.BundleIdentifier) (\(Config.Version); OS \(Config.OS))"
+  ]
+
   private let SuccessStatusCodes = 200..<300
 
-  private let session: NSURLSession
+  private let manager: Alamofire.Manager
 
   init() {
-    func makeHeaders() -> [String: String] {
-      return [
-        "Accept-Encoding": "gzip,compress;q=0.9",
-        "User-Agent": "\(Config.Executable)/\(Config.BundleIdentifier) (\(Config.Version); OS \(Config.OS))"
-      ]
-    }
-
     func makeConfig() -> NSURLSessionConfiguration {
       let config = NSURLSessionConfiguration.ephemeralSessionConfiguration()
       config.allowsCellularAccess = true
       config.timeoutIntervalForRequest = 60
       config.timeoutIntervalForResource = 60 * 60 * 10
-      config.HTTPAdditionalHeaders = makeHeaders()
       return config
     }
 
-    func makeOperationQueue() -> NSOperationQueue {
-      let operationQueue = NSOperationQueue()
-      operationQueue.qualityOfService = .UserInitiated
-      operationQueue.maxConcurrentOperationCount = 6
-      return operationQueue
-    }
-
-    session = NSURLSession(
-      configuration: makeConfig(),
-      delegate: nil,
-      delegateQueue: makeOperationQueue())
+    manager = Alamofire.Manager(configuration: makeConfig())
   }
 
-  func request(method: Method, url: NSURL, headers: Headers = [:]) -> Future<NSHTTPURLResponse> {
-    let request = makeURLRequest(url: url, method: method, headers: headers)
-    return promisifyTask { promise in
-      return self.session.dataTaskWithRequest(request) { data, response, error in
-        if error != nil {
-          promise.reject(Config.Net.GenericErrorDescription)
-        } else if let res = response as? NSHTTPURLResponse {
-          if self.isSuccessStatusCode(res.statusCode) {
-            promise.resolve(res)
+  func request(method: Method, url: NSURL, headers: Headers = [:])
+    -> Future<NSHTTPURLResponse>
+  {
+    let request = makeRequest(url: url, method: method, headers: headers)
+    let promise = Future<NSHTTPURLResponse>.promise()
+    Alamofire
+      .request(request)
+      .response(
+        queue: QueueExecution.backgroundQueue,
+        serializer: Alamofire.Request.responseDataSerializer(),
+        completionHandler: { request, response, data, error in
+          if error != nil {
+            promise.reject(Config.Net.GenericErrorDescription)
+          } else if let res = response {
+            if self.isSuccessStatusCode(res.statusCode) {
+              promise.resolve(res)
+            } else {
+              promise.reject(res)
+            }
           } else {
-            promise.reject(res)
+            promise.reject("Unknown request error for \(method) \(url)")
           }
-        } else {
-          promise.reject("Unknown request error for \(method) \(url)")
-        }
-      }
-    }
+        })
+    return promise
   }
 
-  func head(url: NSURL, headers: Headers = [:]) -> Future<NSHTTPURLResponse> {
-    return request(.Head, url: url, headers: headers)
+  func head(url: NSURL, headers: Headers = [:]) -> Future<NSHTTPURLResponse>
+  {
+    return request(.HEAD, url: url, headers: headers)
   }
 
-  func download(url: NSURL, to destination: NSURL, headers: Headers = [:]) -> Future<NSURL> {
-    let request = makeURLRequest(url: url, headers: headers)
-    return promisifyTask { promise in
-      return self.session.downloadTaskWithRequest(request) { location, response, error in
-        if error != nil {
-          promise.reject(Config.Net.GenericErrorDescription)
-        } else if let res = response as? NSHTTPURLResponse {
-          if self.isSuccessStatusCode(res.statusCode) {
-            Files.move(from: location, to: destination)
-            promise.resolve(destination)
+  func download(url: NSURL, to destination: NSURL, headers: Headers = [:])
+    -> Future<NSURL>
+  {
+    let request = makeRequest(url: url, headers: headers)
+    let promise = Future<NSURL>.promise()
+    Alamofire
+      .download(request, { _, _ in destination })
+      .response(
+        queue: QueueExecution.backgroundQueue,
+        serializer: Alamofire.Request.responseDataSerializer(),
+        completionHandler: { request, response, _, error in
+          if error != nil {
+            promise.reject(Config.Net.GenericErrorDescription)
+          } else if let res = response {
+            if self.isSuccessStatusCode(res.statusCode) {
+              promise.resolve(destination)
+            } else {
+              promise.reject(res)
+            }
           } else {
-            promise.reject(res)
+            promise.reject("Unknown download error for \(url)")
           }
-        } else {
-          promise.reject("Unknown download error for \(url)")
-        }
-      }
-    }
+        })
+    return promise
   }
 
-  private func makeURLRequest(#url: NSURL, method: Method = .Get, headers: Headers = [:]) -> NSURLRequest {
+  private func makeRequest(
+    #url: NSURL,
+    method: Method = .GET,
+    headers: Headers = [:])
+    -> NSURLRequest
+  {
     let request = NSMutableURLRequest(URL: url)
-    request.HTTPMethod = method.description
+    request.HTTPMethod = method.rawValue
+    for (headerName, headerValue) in DefaultHeaders {
+      request.setValue(headerValue, forHTTPHeaderField: headerName)
+    }
     for (headerName, headerValue) in headers {
       request.setValue(headerValue, forHTTPHeaderField: headerName)
     }
     return request
   }
 
-  private func promisifyTask<T>(
-    block: PromiseFuture<T> -> NSURLSessionTask)
-    -> Future<T>
-  {
-    let promise = Future<T>.promise()
-    let task = block(promise)
-    task.resume()
-    return promise
-  }
-
   private func isSuccessStatusCode(code: Int) -> Bool {
     return contains(SuccessStatusCodes, code)
   }
 
-  enum Method: Printable {
-    case Head
-    case Get
-
-    var description: String {
-      switch self {
-      case Head:
-        return "HEAD"
-      case Get:
-        return "GET"
-      }
-    }
+  enum Method: String {
+    case HEAD = "HEAD"
+    case GET = "GET"
   }
 }
