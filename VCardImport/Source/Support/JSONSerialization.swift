2ed8fcaa4a000940a25b440566ab03915c96902b
Tuomas Kareinen <tkareine@gmail.com>
2015-11-09 22:20:20 +0200

    Simplify code leftover from Swift 1.2 to 2.0 migration

--- VCardImport/Source/Support/JSONSerialization.swift
+++ VCardImport/Source/Support/JSONSerialization.swift
@@ -2,24 +2,18 @@ import Foundation
 
 struct JSONSerialization {
   static func encode(obj: AnyObject) -> NSData {
-    var err: NSError?
     do {
-      let data = try NSJSONSerialization.dataWithJSONObject(obj, options: [])
-      return data
-    } catch let error as NSError {
-      err = error
-      fatalError("JSON serialization failed: \(err!)")
+      return try NSJSONSerialization.dataWithJSONObject(obj, options: [])
+    } catch {
+      fatalError("JSON serialization failed: \(error)")
     }
   }
 
   static func decode(data: NSData) -> AnyObject {
-    var err: NSError?
     do {
-      let obj: AnyObject = try NSJSONSerialization.JSONObjectWithData(data, options: [])
-      return obj
-    } catch let error as NSError {
-      err = error
-      fatalError("JSON deserialization failed: \(err!)")
+      return try NSJSONSerialization.JSONObjectWithData(data, options: [])
+    } catch {
+      fatalError("JSON deserialization failed: \(error)")
     }
   }
 }
